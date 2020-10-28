a74923a63c0cbb8484db7f08ddb21ca300665835
YHOSHI53 <amazonzoku39@gmail.com>
2020-10-28 16:41:12 +0900

    leaf

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue ({
+  el: '#app', 
+  data: {
+    active: false,
+    navi: false,
+  },
+  methods: {
+    naviOpen: function() {
+      this.active = !this.active;
+      this.navi = !this.navi;
+    }
+  }
+})
